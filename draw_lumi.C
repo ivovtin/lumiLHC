d4c95e047ae9f57be4e5baa624a3c0f338cbe021
ivovtin <ivan.ovtin@cern.ch>
2021-11-01 21:11:46 +0100

    delete TPad

--- draw_lumi.C
+++ draw_lumi.C
@@ -17,35 +17,29 @@ void draw_lumi(Int_t t1=0, Int_t t2=0, TString LumInFile="lum0.csv")
 {
   gStyle->SetTimeOffset(0);
   gStyle->SetOptStat(0);
+  gStyle->SetPadTickY(1);
+  gStyle->SetPadGridX(1);
+  gStyle->SetPadGridY(1);
+  gStyle->SetPadTopMargin(0.1);
+  gStyle->SetPadLeftMargin(0.1); 
+  gStyle->SetPadBottomMargin(0.1);
 
-  TCanvas *cc = new TCanvas("cc","cc",1200,900);
+  TCanvas *cc = new TCanvas("cc","cc",1200,600);
   cc->Range(0,0,1,1);
 
   float r = 0.35;
-  float epsilon = 0.02;
-
-  TPad PadLum("Pad1", "Pad1", 0., 0., 1., r*(1-epsilon) );
-  PadLum.cd();
-  gPad->SetTicky();
-  gPad->SetGridx();
-  gPad->SetGridy();
-
-  PadLum.SetTopMargin(0);
-  PadLum.SetBottomMargin(0.2);
-  PadLum.Draw();
-  PadLum.cd();
 
   TH2D hax("hax",";date (day/month/year);#splitline{LHC luminosity  }{(10^{33} cm^{-2} s^{-1})  }",10,t1, t2,6,0.,0.02);
   hax.GetXaxis()->SetTimeDisplay(1);
   hax.GetXaxis()->SetTimeFormat("%d/%m/%y");
   hax.GetXaxis()->SetNdivisions(-205);
   hax.GetYaxis()->SetNdivisions(-205);
-  hax.GetYaxis()->SetLabelSize(0.04*(1-r)/r);
-  hax.GetXaxis()->SetLabelSize(0.04*(1-r)/r);
-  hax.GetXaxis()->SetTitleSize(0.04*(1-r)/r);
-  hax.GetYaxis()->SetTitleSize(0.04*(1-r)/r);
-  hax.GetYaxis()->SetTitleOffset(0.5);
-  hax.GetXaxis()->SetTitleOffset(1.3);
+  hax.GetYaxis()->SetLabelSize(0.02*(1-r)/r);
+  hax.GetXaxis()->SetLabelSize(0.02*(1-r)/r);
+  hax.GetXaxis()->SetTitleSize(0.02*(1-r)/r);
+  hax.GetYaxis()->SetTitleSize(0.02*(1-r)/r);
+  hax.GetYaxis()->SetTitleOffset(1.25);
+  hax.GetXaxis()->SetTitleOffset(1.25);
   hax.GetXaxis()->SetLabelOffset(0.03);
   hax.Draw("AXIS");
 
@@ -56,10 +50,13 @@ void draw_lumi(Int_t t1=0, Int_t t2=0, TString LumInFile="lum0.csv")
   lumi1->SetMarkerColor(38);
   lumi1->Draw("lumi:time",Form("time>%d && time<%d",t1, t2 ),"same");
 
-  PadLum.Update();
-  PadLum.Modified();
+  //PadLum.Update();
+  //PadLum.Modified();
   cc->cd();
 
-  PadLum.SaveAs("/eos/user/i/ivovtin/LM_ECAL/RUN3/lumi_draw.eps");
+  //PadLum.SaveAs("/eos/user/i/ivovtin/LM_ECAL/RUN3/lumi_draw.eps");
   //PadLum.SaveAs("/eos/user/i/ivovtin/LM_ECAL/RUN3/lumi_draw.png");
+  cc->SaveAs("/eos/user/i/ivovtin/LM_ECAL/RUN3/lumi_draw.eps");
+  cc->SaveAs("/eos/user/i/ivovtin/LM_ECAL/RUN3/lumi_draw.png");
+  //gPad->Print("/eos/user/i/ivovtin/LM_ECAL/RUN3/lumi_draw.png");
 }
